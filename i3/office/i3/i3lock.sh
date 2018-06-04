933284e6b637fb89a5af552dd59712feeca5cfff
Julien Chevrier <julien.chevrier.github@gmail.com>
2018-06-04 09:40:58 +0200

    i3: Add office desktop configuration
    Add initial i3 configuration from office desktop.
    
    Signed-off-by: Julien Chevrier

---
+++ i3/office/i3/i3lock.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#scrot /tmp/screen_locked.png
+#convert /tmp/screen_locked.png -blur 2x2 /tmp/screen_locked2.png
+#i3lock -i /tmp/screen_locked2.png
+i3lock -t -i ~/Pictures/wall_e_1920_1200.png
+
